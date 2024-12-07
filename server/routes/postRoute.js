2456f6af77f9f2604a8831a214000cf374d6ee89
Snkenim <snekim288@gmail.com>
2024-12-07 09:08:54 +0800

    Fix

--- server/routes/postRoute.js
+++ server/routes/postRoute.js
@@ -1,10 +1,18 @@
 const { Router } = require("express")
 const { createPost, getPost, deletePost } = require("../controllers/postControl")
-
+const postModel = require("../database/schema/postModel")
 const postRoute = Router()
 
 postRoute.post('/createPost', createPost)
 postRoute.get('/findPost', getPost)
 postRoute.delete('/deletePost', deletePost)
+postRoute.get("/posts", async(req, res) => {
+    try {
+        const posts = await postModel.find();
+        res.send(posts)
+    } catch (error){
+        res.send(error)
+    }
+})
 
 module.exports = postRoute
