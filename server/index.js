3b7846149e37eb4495d0e4f454e6da4185106a3a
Snkenim <snekim288@gmail.com>
2024-12-10 14:16:06 +0800

    Updated cloud

--- server/index.js
+++ server/index.js
@@ -1,10 +1,19 @@
 const express = require('express')
 const cors = require('cors')
 const connectDatabase = require('./utils/connectToDb.js')
+const cloudinary = require('cloudinary').v2
 const postRoute = require('./routes/postRoute.js')
 const userRoute = require('./routes/userRoute.js')
 const commentRoute = require("./routes/commentRoute.js")
 const likePost = require("./routes/likeRoute.js")
+
+require('dotenv').config();
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const PORT = 8080
  
