5209fe1cef847822023da5d84634d5c4041d698d
Snkenim <snekim288@gmail.com>
2024-12-16 18:35:26 +0800

    patches 3.0

--- server/database/schema/postModel.js
+++ server/database/schema/postModel.js
@@ -1,11 +1,11 @@
 const { mongoose, Schema } = require('mongoose')
  
 const postSchema = new Schema({
-    UserId: [{ type: Schema.Types.ObjectId, ref: 'users', required: true}],
+    UserId: [{ type: Schema.Types.ObjectId, ref: 'users', required: true }],
     postImg: String,
     caption: String,
-    likes: {type: Array, default:[]},
-    comments: [{ type: Schema.Types.ObjectId, ref: 'comments'}]
+    likes: { type: Array, default: [] },
+    comments: [{ type: Schema.Types.ObjectId, ref: 'comments' }]
  
 })
  
